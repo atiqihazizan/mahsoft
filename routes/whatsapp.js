061081c108e9d9c69391a3397ec6d821ffbdd6f6
atiqihazizan <aqi852000@gmail.com>
2026-07-14 05:58:12 +0800

    chore: remove stray files from tracking

--- routes/whatsapp.js
+++
@@ -1,9 +0,0 @@
-const encodeText = (text) => encodeURIComponent(text)
-
-const generateWaLink = (phone, message) => {
-  const cleaned = phone.replace(/[^0-9]/g, '')
-  if (cleaned.length < 10) return null
-  return `https://wa.me/${cleaned}?text=${encodeText(message)}`
-}
-
-module.exports = { generateWaLink }
